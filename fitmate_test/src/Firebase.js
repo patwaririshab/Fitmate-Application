3e3830ce042bbf9337b012c2a452e70965a30162
P Pradeep <38093268+Pdotdeep@users.noreply.github.com>
2019-05-28 18:03:20 +0800

    firestore

---
+++ fitmate_test/src/Firebase.js
@@ -0,0 +1,8 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+import { firebaseConfig } from './firebaseconfig'
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- fitmate_test/src/firebaseconfig.js
+++ fitmate_test/src/firebaseconfig.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCigcPmpzEu-n7MhLPXDmBMCuHM8dmfQQo",
-  authDomain: "fitmate-react.firebaseapp.com",
-  databaseURL: "https://fitmate-react.firebaseio.com",
-  projectId: "fitmate-react",
-  storageBucket: "fitmate-react.appspot.com",
+  apiKey: "AIzaSyBbiuykY8xcEVeo4fRvftnH6-2G4x-bl2w",
+  authDomain: "fit-mate-8457f.firebaseapp.com",
+  databaseURL: "https://fit-mate-8457f.firebaseio.com",
+  projectId: "fit-mate-8457f",
+  storageBucket: "fit-mate-8457f.appspot.com",
 };
