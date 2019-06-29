ab249e773e807b360cd6c6403920a92100815446
Rishab Patwari <e0311251@u.nus.edu>
2019-06-29 19:51:31 +0800

    CameraRoll Storage bug on first launch resolved

--- fitmate_test/src/screens/UploadScreen/uploadScreen.js
+++ fitmate_test/src/screens/UploadScreen/uploadScreen.js
@@ -4,9 +4,6 @@ import * as firebase from 'firebase';
 import 'firebase/storage'
 import axios from 'react-native-axios';
 
-
-const { app } = firebase.storage();
-
 class uploadScreen extends Component {
     constructor(props) {
         super(props);
@@ -25,65 +22,21 @@ class uploadScreen extends Component {
         })
     };
 
-
-
-uploadHandlerTest2 = () => {
-    var storageRef = firebase.storage().ref();
-    var pushUpRef = storageRef.child('PushUp1.mp4');
-    var pushUpVideosRef = storageRef.child('pushUpVideos/PushUp1.mp4')
-    
-    var file = {
-        uri: this.props.uri,
-        name: 'pushups.mp4',
-        type: 'videos/mp4'
-    }
-
-    var uploadTask = storageRef.child('videos/pushup.mp4').put(file);
-    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,function(snapshot) {    
-                    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    console.log('Upload is ' + progress + '% done');
-                    switch (snapshot.state) {
-                      case firebase.storage.TaskState.PAUSED: // or 'paused'
-                        console.log('Upload is paused');
-                        break;
-                      case firebase.storage.TaskState.RUNNING: // or 'running'
-                        console.log('Upload is running');
-                        break;
-                    }
-                }, 
-                error => {
-                    switch (error.code) {
-                        case 'storage/unauthorized' :
-                            console.log('Storage Unauthorized');
-                            break;
-                        
-                        case 'storage/canceled' :
-                            console.log('Storage Canceled');
-                            break;
-                        case 'storage/unknown' :
-                            console.log('Storage Unknown');
-                            break;
-                    }
-                }, 
-                function() {
-                    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                        console.log('File available at', downloadURL);
-                    });
-                });
-    };
-
-
-    uploadHandler = () => {
-        
+        uploadHandler = () => {
         var storageRef = firebase.storage().ref();
+        //var file = new Blob (["/sdcard/DCIM/a956d98c-b27d-4fd7-aad9-14207f98fd8d.mp4"],{"type": "video\/mp4"});
+        var file = new Blob(["file:///data/user/0/com.fitmate_test/cache/Camera/391b307a-6cc0-4d43-aaf5-80373d02c349.mp4"],{"type": "video\/mp4"})
+        //var value = URL.createObjectURL(file);
 
-        var file = new Blob (
-            [this.props.uri],
-            {"type": "video\/mp4"}
-            );
+        // let formData = new FormData();
+        // formData.append("videoFile", {
+        //     name: video1.mp4,
+        //     uri: this.props.uri,
+        //     type: 'video/mp4'
+        // })
         
-        firebase.storage().ref()
-            .child('videos/')
+        storageRef
+            .child('videos/video1.mp4')
             .put(file)
             .on(firebase.storage.TaskEvent.STATE_CHANGED,
                 function(snapshot) {    
@@ -111,33 +64,40 @@ uploadHandlerTest2 = () => {
                             console.log('Storage Unknown');
                             break;
                     }
-                }, 
-                function() {
-                    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                        console.log('File available at', downloadURL);
-                    });
-                });
+                }); 
+                // function() {
+                //     uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                //         console.log('File available at', downloadURL);
+                //     });
+                // });
+                // }
         }
+
+
+    
     saveToCameraRoll = () => {
-        requestExternalStoragePermission = async () => {
+        requestExternalStoragePermission = async  () => {
             try {
               const granted = await PermissionsAndroid.request(
                 PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
                 {
                   title: 'My App Storage Permission',
                   message: 'My App needs access to your storage ' +
-                    'so you can save your photos',
+                    'so you can save your video',
                 },
               );
               console.log('DONT HAVE PERMISSION ERROR');
+              console.log(this.props.uri);
+              CameraRoll.saveToCameraRoll(this.props.uri)
               return granted;
             } catch (err) {
               console.error('Failed to request permission ', err);
+              alert('Something went wrong')
               return null;
             }
           };
         requestExternalStoragePermission();
-        CameraRoll.saveToCameraRoll(this.props.uri);        
+        
     }
 
    uploadBackend = () => {
@@ -171,3 +131,79 @@ const styles = StyleSheet.create({
     
 })
 export default uploadScreen
+
+
+
+// uploadHandlerTest2 = () => {
+//     var storageRef = firebase.storage().ref();
+//     var pushUpRef = storageRef.child('PushUp1.mp4');
+//     var pushUpVideosRef = storageRef.child('pushUpVideos/PushUp1.mp4')
+    
+//     var file = {
+//         uri: this.props.uri,
+//         name: 'pushups.mp4',
+//         type: 'videos/mp4'
+//     }
+
+//     var uploadTask = storageRef.child('videos/pushup.mp4').put(file);
+//     uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,function(snapshot) {    
+//                     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//                     console.log('Upload is ' + progress + '% done');
+//                     switch (snapshot.state) {
+//                       case firebase.storage.TaskState.PAUSED: // or 'paused'
+//                         console.log('Upload is paused');
+//                         break;
+//                       case firebase.storage.TaskState.RUNNING: // or 'running'
+//                         console.log('Upload is running');
+//                         break;
+//                     }
+//                 }, 
+//                 error => {
+//                     switch (error.code) {
+//                         case 'storage/unauthorized' :
+//                             console.log('Storage Unauthorized');
+//                             break;
+                        
+//                         case 'storage/canceled' :
+//                             console.log('Storage Canceled');
+//                             break;
+//                         case 'storage/unknown' :
+//                             console.log('Storage Unknown');
+//                             break;
+//                     }
+//                 }, 
+//                 function() {
+//                     uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+//                         console.log('File available at', downloadURL);
+//                     });
+//                 });
+//     };
+
+
+uploadHandler = () => {
+        const data = new FormData;
+        data.append('video', {
+            uri: this.props.uri,
+            type: 'video/mp4',
+            name: 'pushUpVideo'
+        });
+
+        axios.post('https://us-central1-fit-mate-8457f.cloudfunctions.net/uploadFile',
+            data, 
+            {headers: {'Content-Type': 'multipart/form-data'}}
+        )
+        .then(() => console.log('SUCCESS!!'))
+        .catch(() => console.log('FAILURE!!'));
+        
+        // fetch('https://us-central1-fit-mate-8457f.cloudfunctions.net/uploadFile', {
+        //     method: 'POST',
+        //     headers: { 
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'multipart/form-data',
+        //     },
+        //     body: data,
+        //     })
+        //     .then((response) => console.log(response)).catch((error) => {
+        //         console.log(error)
+        //     });
+    }
